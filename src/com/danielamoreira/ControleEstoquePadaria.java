8f731a1134bd4d59ac7cb65c05f44e330808a4b1
Daniela Alves <dani.alvesm22@gmail.com>
2025-10-25 22:27:32 -0300

    feat: add java exercise

---
+++ src/com/danielamoreira/ControleEstoquePadaria.java
@@ -0,0 +1,12 @@
+package com.danielamoreira;
+
+public class ControleEstoquePadaria {
+    public int quantidadeFarinha = 10;
+    int quantidadeAcucar = 5;
+
+    void main() {
+        System.out.println("Quantidade de farinha: " + quantidadeFarinha);
+        System.out.println("Quantidade de açúcar: " + quantidadeAcucar);
+    }
+
+}
